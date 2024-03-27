38d647214a67791da415f6cdf70bbc8c989f13a4
g.turri <g.turri@criteo.com>
2024-03-27 23:06:28 +0100

    Two characters moving in opposite direction

--- test1/test1MainNode.gd
+++ test1/test1MainNode.gd
@@ -1,13 +1,34 @@
 extends Node2D
 
-var speed = 0.1
+@export var characterScene: PackedScene
+
+var character1
+var ratioCharacter1 = 0
+var speed1 = 0.1
+
+var character2
+var ratioCharacter2 = 0.5
+var speed2 = -0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	character1 = buildCharacter(Color(0, 0, 1))
+	add_child(character1)
+	character2 = buildCharacter(Color(0, 1, 0))
+	add_child(character2)
 
+func buildCharacter(color):
+	var character = characterScene.instantiate()
+	character.modulate = color
+	return character
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Path2D/PathFollow2D.progress_ratio += speed * delta
-	$Area2D.position = $Path2D/PathFollow2D.position
+	ratioCharacter1 += speed1 * delta
+	$Path2D/PathFollow2D.progress_ratio = ratioCharacter1
+	character1.position = $Path2D/PathFollow2D.position
+
+	ratioCharacter2 += speed2 * delta
+	$Path2D/PathFollow2D.progress_ratio = ratioCharacter2
+	character2.position = $Path2D/PathFollow2D.position
+
