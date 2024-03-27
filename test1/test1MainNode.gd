5b1b73155c2841851696b2a9c444239150180b8e
g.turri <g.turri@criteo.com>
2024-03-27 23:24:22 +0100

    handle collisions

--- test1/test1MainNode.gd
+++ test1/test1MainNode.gd
@@ -1,34 +1,34 @@
 extends Node2D
 
+signal hit
+
 @export var characterScene: PackedScene
 
 var character1
-var ratioCharacter1 = 0
-var speed1 = 0.1
-
 var character2
-var ratioCharacter2 = 0.5
-var speed2 = -0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	character1 = buildCharacter(Color(0, 0, 1))
+	character1 = buildCharacter(0.1, 0, Color(0, 0, 1))
 	add_child(character1)
-	character2 = buildCharacter(Color(0, 1, 0))
+	character2 = buildCharacter(-0.1, 0.5, Color(0, 1, 0))
 	add_child(character2)
 
-func buildCharacter(color):
+func buildCharacter(speed, ratio, color):
 	var character = characterScene.instantiate()
 	character.modulate = color
+	character.speed = speed
+	character.ratio = ratio
 	return character
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	ratioCharacter1 += speed1 * delta
-	$Path2D/PathFollow2D.progress_ratio = ratioCharacter1
+	character1.ratio += character1.speed * delta
+	$Path2D/PathFollow2D.progress_ratio = character1.ratio
 	character1.position = $Path2D/PathFollow2D.position
-
-	ratioCharacter2 += speed2 * delta
-	$Path2D/PathFollow2D.progress_ratio = ratioCharacter2
+	
+	character2.ratio += character2.speed * delta
+	$Path2D/PathFollow2D.progress_ratio = character2.ratio
 	character2.position = $Path2D/PathFollow2D.position
+
 
