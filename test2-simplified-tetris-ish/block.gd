12a61a55e8032c16025ba8a545bc6ae7fee6152c
g.turri <g.turri@criteo.com>
2024-03-30 23:07:35 +0100

    blocks pile up nicely

--- test2-simplified-tetris-ish/block.gd
+++ test2-simplified-tetris-ish/block.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-signal reached_bottom
+signal overlap
+
+var size = 20 # must match the size of the collision box and the sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,5 +13,13 @@ func _ready():
 func _process(delta):
 	pass
 
+func get_position_of_upper_side():
+	return position.y - size / 2
+
+func get_position_of_lower_side():
+	return position.y + size / 2
+
+func set_position_of_lower_side(y):
+	position.y = y - size / 2
 func _on_area_entered(area):
-	reached_bottom.emit(self)
+	overlap.emit(self)
