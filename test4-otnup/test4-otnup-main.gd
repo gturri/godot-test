5e92c37c7bc3c7ed1e584b35ee4bf9b37ddc51b7
Guillaume Turri <g.turri@criteo.com>
2024-04-07 23:39:23 +0200

    Check some validity rules before putting a card

--- test4-otnup/test4-otnup-main.gd
+++ test4-otnup/test4-otnup-main.gd
@@ -4,11 +4,14 @@ var deckScene = preload("res://test4-otnup/Deck.tscn")
 var decks = Array()
 var currentPlayer = 0
 var numberPlayers = 2
+var cardsPlayed = {}
+var nextCard
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	createDeck()
 	createDeck()
+	drawNextCard()
 
 func createDeck():
 	var deck = deckScene.instantiate()
@@ -19,11 +22,37 @@ func _input(event):
 	if event.is_action_pressed("select_tile"):
 		var mouse_pos = get_global_mouse_position()
 		var tile_pos = $TileMap.local_to_map(mouse_pos)
-		var card = decks[currentPlayer].draw()
-		var tile = Vector2i(card, currentPlayer)
-		print("tempGT: set_cell with tile " + str(tile))
+		if not canPutCard(tile_pos):
+			return
+		var tile = Vector2i(nextCard, currentPlayer)
 		$TileMap.set_cell(0, tile_pos, 1, tile)
+		# TODO: check if the player won, or if there are no cards left
+		cardsPlayed[tile_pos] = [nextCard, currentPlayer] #TODO: could be cleaner to have an ad hoc class instead of this Array
 		currentPlayer = (currentPlayer + 1) % numberPlayers
+		drawNextCard()
+
+func drawNextCard():
+	nextCard = decks[currentPlayer].draw()
+	# TODO: display this message in-game
+	print("next card (for player " + str(currentPlayer) + "): " + str(nextCard))
+
+func canPutCard(tile_pos):
+	if cardsPlayed.is_empty():
+		return true
+	if cardsPlayed.has(tile_pos):
+		var existingCard = cardsPlayed[tile_pos]
+		if existingCard[1] == currentPlayer:
+			# TODO: display this message in-game
+			print("ERR: a player cannot play on his or her own card")
+			return false
+		if nextCard > existingCard[0]:
+			return true
+		else:
+			print("ERR: card is not bigger than the existing one")
+			return false
+	# TODO: check that we're near an existing card
+	# TODO: check we're in a 6x6 square
+	return true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
