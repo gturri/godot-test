31d462f002dafe0100dec0ce301e1cd29421bfd2
Guillaume Turri <g.turri@criteo.com>
2024-04-07 22:06:34 +0200

    Re-numerotate the tileset source id
    To avoid having this semantic-less "2" magic number (which was probably
    the result of trying several times to import the tileset).
    
    nb: FWIW, I could not find a way to change this id from Godot IDE, I had
    to manually change the underlying file

--- test4-otnup/test4-otnup-main.gd
+++ test4-otnup/test4-otnup-main.gd
@@ -9,7 +9,7 @@ func _input(event):
 	if event.is_action_pressed("select_tile"):
 		var mouse_pos = get_global_mouse_position()
 		var tile_pos = $TileMap.local_to_map(mouse_pos)
-		$TileMap.set_cell(0, tile_pos, 2, Vector2i(0, 0))
+		$TileMap.set_cell(0, tile_pos, 0, Vector2i(0, 0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
