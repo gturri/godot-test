25569f6010ba22a326b1bfeef41a660e6c00e93c
Guillaume Turri <g.turri@criteo.com>
2024-04-07 21:51:44 +0200

    Use InputMap instead of hardcoded keys

--- test4-otnup/test4-otnup-main.gd
+++ test4-otnup/test4-otnup-main.gd
@@ -6,11 +6,11 @@ func _ready():
 	pass # Replace with function body.
 
 func _input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		#print("tempGT: got click event")
+	if event.is_action_pressed("select_tile"):
 		var mouse_pos = get_global_mouse_position()
 		var tile_pos = $TileMap.local_to_map(mouse_pos)
 		print("tempGT: " + str(mouse_pos) + " --> " + str(tile_pos))
+		$TileMap.set_cell(0, tile_pos, 0, Vector2i(1, 1))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
