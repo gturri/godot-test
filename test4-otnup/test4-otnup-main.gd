c824eb4ebe2ce3ae2104466ee83e0ba6240a9e05
Guillaume Turri <g.turri@criteo.com>
2024-04-08 22:47:21 +0200

    tiles must be in a 6x6 square

--- test4-otnup/test4-otnup-main.gd
+++ test4-otnup/test4-otnup-main.gd
@@ -6,6 +6,12 @@ var currentPlayer = 0
 var numberPlayers = 2
 var cardsPlayed = {}
 var nextCard
+static var maxBoardDimension = 6
+
+var xmin = null;
+var xmax = null;
+var ymin = null;
+var ymax = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,11 +31,29 @@ func _input(event):
 		if not canPutCard(tile_pos):
 			return
 		var tile = Vector2i(nextCard, currentPlayer)
+		updateBoardDimension(tile_pos)
 		$TileMap.set_cell(0, tile_pos, 1, tile)
 		# TODO: check if the player won, or if there are no cards left
 		cardsPlayed[tile_pos] = [nextCard, currentPlayer] #TODO: could be cleaner to have an ad hoc class instead of this Array
 		currentPlayer = (currentPlayer + 1) % numberPlayers
 		drawNextCard()
+
+func updateBoardDimension(tile_pos):
+	if xmin == null:
+		xmin = tile_pos.x
+		xmax = tile_pos.x
+		ymin = tile_pos.y
+		ymax = tile_pos.y
+		return
+	if xmin > tile_pos.x:
+		xmin = tile_pos.x
+	if xmax < tile_pos.x:
+		xmax = tile_pos.x
+	if ymin > tile_pos.y:
+		ymin = tile_pos.y
+	if ymax < tile_pos.y:
+		ymax = tile_pos.y
+	print("board limits: x=[" + str(xmin) + ", " + str(xmax) + "], y=[" + str(ymin) + ", " + str(ymax) + "]")
 
 func drawNextCard():
 	nextCard = decks[currentPlayer].draw()
@@ -39,6 +63,9 @@ func drawNextCard():
 func canPutCard(tile_pos):
 	if cardsPlayed.is_empty():
 		return true
+	if not inBoardLimits(tile_pos):
+		print("not in board limit")
+		return false
 	if cardsPlayed.has(tile_pos):
 		var existingCard = cardsPlayed[tile_pos]
 		if existingCard[1] == currentPlayer:
@@ -51,8 +78,14 @@ func canPutCard(tile_pos):
 			print("ERR: card is not bigger than the existing one")
 			return false
 	# TODO: check that we're near an existing card
-	# TODO: check we're in a 6x6 square
 	return true
+
+func inBoardLimits(tile_pos):
+	print("checking boardLimits with " + str(tile_pos))
+	return tile_pos.x - xmin < maxBoardDimension and \
+		xmax - tile_pos.x < maxBoardDimension and \
+		tile_pos.y - ymin < maxBoardDimension and \
+		ymax - tile_pos.y < maxBoardDimension;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
