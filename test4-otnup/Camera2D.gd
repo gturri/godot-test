c00eb02c749ed0d4933c929bee93b152a97db7a8
Guillaume Turri <g.turri@criteo.com>
2024-04-11 23:26:24 +0200

    Can zoom-in and zoom-out

--- test4-otnup/Camera2D.gd
+++ test4-otnup/Camera2D.gd
@@ -1,5 +1,6 @@
 extends Camera2D
 
+var zoom_factor = 0.1
 var moveCamera: bool = false
 var previousPosition: Vector2
 
@@ -12,5 +13,9 @@ func _unhandled_input(event: InputEvent):
 			moveCamera = false
 	elif event is InputEventMouseMotion && moveCamera:
 		# get_tree().set_input_as_handled(); # TODO: understand if we need this line (which comes from the snippet on https://forum.godotengine.org/t/how-to-drag-camera-with-mouse/28508/2 )
-		position += (previousPosition - event.position)
+		position += (previousPosition - event.position) / zoom
 		previousPosition = event.position
+	elif event.is_action_pressed("zoom_in"):
+		zoom *= 1+zoom_factor
+	elif event.is_action_pressed("zoom_out"):
+		zoom *= 1-zoom_factor
