d9acfe2e1e22efecc98d5669ac5ec9f5314e01bf
Guillaume Turri <g.turri@criteo.com>
2024-04-14 23:08:17 +0200

    First skeleton for HUD

---
+++ test4-otnup/hud.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+
+signal startGame
+
+func showHintMessage(text) -> void:
+	$HintMessage.text = text
+	$HintMessage.show()
+	await get_tree().create_timer(2.0).timeout
+	$HintMessage.hide()
+
+func onGameDraw() -> void:
+	$MainMessage.text = "Draw"
+	$MainMessage.show()
+
+func onPlayerWon(player: int) -> void:
+	$MainMessage.text = "Player " + str(player) + " won!"
+	$MainMessage.show()
+
+func __gameEnded() -> void:
+	$NewGameButton.show()
+
+func _on_new_game_button_pressed():
+	$NewGameButton.hide()
+	$MainMessage.hide()
+	$HintMessage.hide()
+	startGame.emit()
