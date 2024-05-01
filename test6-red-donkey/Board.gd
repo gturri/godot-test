53a39c0f48ba1de3a3381eb1473977de046e5245
Guillaume Turri <g.turri@criteo.com>
2024-05-01 21:08:21 +0200

    Can move the main block

--- test6-red-donkey/Board.gd
+++ test6-red-donkey/Board.gd
@@ -1,5 +1,22 @@
 extends Node2D
- 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+@export var speed = 100
+var currentBlock
+
+func _ready():
+	currentBlock = $BlockTarget
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+
+	if Input.is_action_pressed("right"):
+		direction.x += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
+	if Input.is_action_pressed("down"):
+		direction.y += 1
+	if Input.is_action_pressed("up"):
+		direction.y -= 1
+
+	currentBlock.velocity = speed * direction
+	currentBlock.move_and_slide()
