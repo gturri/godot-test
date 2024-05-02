b3cc9e039ec34a998c4787594135eb0ac51534ec
Guillaume Turri <g.turri@criteo.com>
2024-05-02 22:22:13 +0200

    Refactor: introduce AbstractBlock

--- test6-red-donkey/block_1x_1.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-signal is_selected(selfBlock: CharacterBody2D)
-
-func _on_input_event(_viewport, event, _shape_idx):
-	if event.is_action_pressed("click"):
-		is_selected.emit(self)
