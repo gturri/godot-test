d1e0ae6a98842d4ebc09736ac9d74ff9f5aa1d95
Guillaume Turri <g.turri@criteo.com>
2024-05-02 23:11:43 +0200

    use Input.get_vector to simplify input detection

--- test6-red-donkey/Board.gd
+++ test6-red-donkey/Board.gd
@@ -7,18 +7,7 @@ func _ready():
 	currentBlock = $BlockTarget
 
 func _physics_process(_delta):
-	var direction = Vector2.ZERO
-
-	if Input.is_action_pressed("right"):
-		direction.x += 1
-	if Input.is_action_pressed("left"):
-		direction.x -= 1
-	if Input.is_action_pressed("down"):
-		direction.y += 1
-	if Input.is_action_pressed("up"):
-		direction.y -= 1
-
-	currentBlock.velocity = speed * direction
+	currentBlock.velocity = speed * Input.get_vector("left", "right", "up", "down")
 	currentBlock.move_and_slide()
 
 func _on_goal_area_body_entered(_body):
