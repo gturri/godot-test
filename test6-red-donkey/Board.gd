d6a1f6d385f7bcff913be9b158ce7987b3750c9c
Guillaume Turri <g.turri@criteo.com>
2024-05-02 23:42:55 +0200

    Can move blocks with the mouse

--- test6-red-donkey/Board.gd
+++ test6-red-donkey/Board.gd
@@ -7,7 +7,19 @@ func _ready():
 	currentBlock = $BlockTarget
 
 func _physics_process(_delta):
-	currentBlock.velocity = speed * Input.get_vector("left", "right", "up", "down")
+	# get input from keyboard
+	var direction = Input.get_vector("left", "right", "up", "down")
+
+	# if no key was pressed but the mouse is clicked, get input from mouse
+	if not direction.length() and Input.is_action_pressed("click"):
+		direction = get_global_mouse_position() - currentBlock.position
+
+		# normalize only if the vector is big, to try to avoid oscillations around the cursor
+		# (though it's not enough, there can still be some oscillations)
+		if direction.length() > 1:
+			direction = direction.normalized()
+
+	currentBlock.velocity = speed * direction
 	currentBlock.move_and_slide()
 
 func _on_goal_area_body_entered(_body):
