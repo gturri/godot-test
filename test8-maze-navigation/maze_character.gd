6b66d7417bda0834c16b47aaf06d1620bd05839e
Guillaume Turri <g.turri@criteo.com>
2024-05-05 23:38:20 +0200

    reduce navigation polygons
    to avoid having the sprite stuck on walls

--- test8-maze-navigation/maze_character.gd
+++ test8-maze-navigation/maze_character.gd
@@ -4,8 +4,8 @@ var movement_speed: float = 200.0
 var movement_target_position: Vector2 = Vector2(60.0,180.0)
 
 func _ready():
-	$NavigationAgent2D.path_desired_distance = 30.0
-	$NavigationAgent2D.target_desired_distance = 30.0
+	$NavigationAgent2D.path_desired_distance = 10.0
+	$NavigationAgent2D.target_desired_distance = 1.0
 
 func set_movement_target(movement_target: Vector2):
 	$NavigationAgent2D.target_position = movement_target
