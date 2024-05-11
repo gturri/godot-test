f2939e9f255727645ffaa05845639c933ccc8538
Guillaume Turri <g.turri@criteo.com>
2024-05-11 19:44:18 +0200

    add test11-multi

---
+++ test11-multi/MultiMain.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var port = 6666
+var address = "127.0.0.1"
+
+func _on_set_client_pressed():
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(address, port)
+	multiplayer.multiplayer_peer = peer
+	print("set as client")
+
+func _on_set_server_pressed():
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(port, 5)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_on_player_connected)
+	print("set as server")
+
+func _on_player_connected(id):
+	print("player " + str(id) + " connected")
+
+func _on_send_pressed():
+	received_message.rpc($TextEdit.get_text())
+
+@rpc("any_peer", "reliable", "call_local")
+func received_message(message):
+	$Label.set_text(message)
