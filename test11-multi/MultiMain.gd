6337a1a7a598a37551138813afa1fee4cbd5eff0
Guillaume Turri <g.turri@criteo.com>
2024-06-01 10:24:59 +0200

    backup some files

--- test11-multi/MultiMain.gd
+++ test11-multi/MultiMain.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
 var port = 6666
-var address = "127.0.0.1"
 
 func _on_set_client_pressed():
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_client(address, port)
+	peer.create_client($Address.get_text(), port)
 	multiplayer.multiplayer_peer = peer
 	print("set as client")
 
